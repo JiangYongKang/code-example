feb97c458cd7a5bbdb3c7236210a1f46189c5496
JiangYongKang <vincent4502237@gmail.com>
2017-10-13 17:29:26 +0800

    use gem to manage the code.

---
+++ lib/ruby/code/example.rb
@@ -0,0 +1,9 @@
+require "ruby/code/example/version"
+
+module Ruby
+  module Code
+    module Example
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/ruby/code/example/version.rb
@@ -0,0 +1,7 @@
+module Ruby
+  module Code
+    module Example
+      VERSION = "0.1.0"
+    end
+  end
+end
